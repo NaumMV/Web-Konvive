a3c629d531419b05ad3b17e04a3932843631b1c2
NaumMV <naummv1120@gmail.com>
2025-06-16 20:23:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Muestra en consola que la web cargó correctamente
+  console.log("Sitio Innovation Program activo 🚀");
+
+  // Puedes añadir funciones aquí si más adelante quieres efectos o validaciones
+});
